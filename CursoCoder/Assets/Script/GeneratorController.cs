eb429df1c4737e6137bbc8a3018711c815caf638
Nicodragon25 <98363397+Nicodragon25@users.noreply.github.com>
2022-01-27 00:08:26 -0300

    Multishot
    Added multishot and direction for every spawned bullet

---
+++ CursoCoder/Assets/Script/GeneratorController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorController : MonoBehaviour
+{
+    public GameObject SpawnedObject;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawn(new Vector3(0, 0, 0));
+        }
+        if (Input.GetKeyDown("j"))
+        {
+            Spawn(new Vector3(0,20,0));
+            Spawn(new Vector3(0,-20,0));
+        }
+        if (Input.GetKeyDown("k"))
+        {
+            Spawn(new Vector3(0, 20, 0));
+            Spawn(new Vector3(0, 0, 0));
+            Spawn(new Vector3(0, -20, 0));
+        }
+        if (Input.GetKeyDown("l"))
+        {
+            Spawn(new Vector3(0, 20, 0));
+            Spawn(new Vector3(0, 10, 0));
+            Spawn(new Vector3(0, -10, 0));
+            Spawn(new Vector3(0, -20, 0));
+        }
+    }
+
+    private void Spawn(Vector3 Rotation)
+    {
+        Instantiate(SpawnedObject, transform.position, transform.rotation = Quaternion.Euler(Rotation));
+    }
+}
